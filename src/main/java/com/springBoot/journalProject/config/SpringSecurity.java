70e3348120b0ec14b9b22e8414b3bac6ef224a20
Kirtanbhavsat <Kirtan>
2024-11-29 09:28:47 +0530

    Added new features and fixed bugs

--- src/main/java/com/springBoot/journalProject/config/SpringSecurity.java
+++ src/main/java/com/springBoot/journalProject/config/SpringSecurity.java
@@ -59,8 +59,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -99,4 +101,15 @@ public class SpringSecurity {
     {
         return new BCryptPasswordEncoder();
     }
+
+   /* @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }*/
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
 }

244b66f12dd85a20a47636c67ac37443ece6c92d
Kirtan Bhavsar <kirtanbhavsar21@gmail.com>
2025-02-07 16:46:13 +0530

    refrance code

--- src/main/java/com/springBoot/journalProject/service/EntryService.java
+++ src/main/java/com/springBoot/journalProject/service/EntryService.java
@@ -13,49 +13,40 @@ import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
-public class EntryService
-{
+public class EntryService {
 
     private final PostEntryRepo postEntryRepo;
     private final UserService uservice;
 
 
-    public void saveEntry(PostEntry postEntry, String userName)
-    {
-        try
-        {
+    public void saveEntry(PostEntry postEntry, String userName) {
+        try {
             User user = uservice.findByUserName(userName);
             postEntry.setDate(LocalDateTime.now());
             PostEntry saved = postEntryRepo.save(postEntry);
             user.getEntry().add(saved);
             uservice.saveEntry(user);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             System.out.println(e);
             throw new RuntimeException("An error occurred while saving the entry ",e);
         }
     }
 
 
-    public void saveEntry(PostEntry postEntry)
-    {
+    public void saveEntry(PostEntry postEntry) {
         postEntryRepo.save(postEntry);
     }
 
 
-    public List<PostEntry> getAll()
-    {
+    public List<PostEntry> getAll() {
         return postEntryRepo.findAll();
     }
 
-    public Optional<PostEntry> findById(ObjectId id)
-    {
+    public Optional<PostEntry> findById(ObjectId id) {
         return postEntryRepo.findById(id);
     }
 
-    public boolean deleteById(ObjectId id, String userName)
-    {
+    public boolean deleteById(ObjectId id, String userName) {
         boolean removed = false;
         try {
             User user = uservice.findByUserName(userName);
@@ -64,9 +55,9 @@ public class EntryService
                 uservice.saveEntry(user);
                 postEntryRepo.deleteById(id);
             }
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
+
+
             throw new RuntimeException("An error occurred while deleting the entry.", e);
         }
         return removed;
